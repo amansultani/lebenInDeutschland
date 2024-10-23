b1755512ee070782ea54a7b30de825657d9de0cd
amansultani <aman.sultani09@gmail.com>
2024-10-23 11:35:30 +0200

    view and layout modified

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,32 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+//the following script is used to open and close the menu in mobile view (small display)
+document.addEventListener('DOMContentLoaded', function() {
+
+    const menuButton = document.getElementById('menu-open-button');
+    const closeButton = document.getElementById('menu-close-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+  
+    if (menuButton && closeButton && mobileMenu) {
+       
+        function openMenu() {
+            mobileMenu.classList.remove('hidden');
+        }
+
+    
+        function closeMenu() {
+            mobileMenu.classList.add('hidden');
+        
+        }
+
+       
+        menuButton.addEventListener('click', openMenu);
+
+      
+        closeButton.addEventListener('click', closeMenu);
+    }
+});
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-</head>
-<body>
-  <h1 class="text-3xl font-bold ">
-    Hello world!
-  </h1>
-</body>
-</html>
+<x-layout>
+      
+               
+                <div class="text-center">
+                    <h1 class="text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich
+                        your online business</h1>
+                    <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt.
+                        Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                    
+                </div>
+          
+</x-layout>
